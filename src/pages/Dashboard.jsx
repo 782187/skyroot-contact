6d1fe1b35772d64ca3d4ead8a159a1854faa2f3d
Avinash Shinde <ajs.ind2@gmail.com>
2025-07-11 11:34:09 +0530

    Re Change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,11 +3,21 @@ import ContactList from '../components/Contacts/ContactList';
 import AuthContext from '../context/AuthContext';
 
 const Dashboard = () => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
+    return <div className="text-danger">Unauthorized. Please log in.</div>;
+  }
 
   return (
     <div>
-      <h2 className="text-primary mb-4" style={{WebkitTextStroke: "1px black"}}>Welcome, {user?.username}!</h2>
+      <h2 className="text-primary mb-4" style={{ WebkitTextStroke: '1px black' }}>
+        Welcome, {user.username}!
+      </h2>
       <ContactList />
     </div>
   );
