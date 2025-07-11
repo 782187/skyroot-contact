ee329cec707fde6057f5ad1a1238008157ac6fc6
Avinash Shinde <ajs.ind2@gmail.com>
2025-07-11 12:41:12 +0530

    Re Change mar

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,22 @@ import PrivateRoute from './components/Layout/PrivateRoute';
 
 function App() {
   return (
-    <>
-      <marquee behavior="alternate" scrollamount="10" style="color: red; font-weight: bold; font-size: 18px;">Request, Response may take 20 to 30s. It is hosted on the free version of Render.</marquee>
-
-      <BrowserRouter>
-        <AuthProvider>
-          <Navbar />
-          <div className="container mt-4">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-              <Route path="/contacts/new" element={<PrivateRoute><ContactForm /></PrivateRoute>} />
-              <Route path="/contacts/:id/edit" element={<PrivateRoute><ContactForm /></PrivateRoute>} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
-        </AuthProvider>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <AuthProvider>
+        <Navbar />
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path="/contacts/new" element={<PrivateRoute><ContactForm /></PrivateRoute>} />
+            <Route path="/contacts/:id/edit" element={<PrivateRoute><ContactForm /></PrivateRoute>} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
